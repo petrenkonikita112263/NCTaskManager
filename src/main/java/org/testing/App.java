06be5fc66cfce7da133f64448e56255aa1b24574
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-02-25 10:18:13 +0200

    Исправления [Код] 6: Фикс запуска jar файла, добавлено главый класс который включался при сборке проекта App.java

---
+++ src/main/java/org/testing/App.java
@@ -0,0 +1,32 @@
+package org.testing;
+
+import controller.MajorController;
+
+import java.io.IOException;
+
+/**
+ * Main class that run MVC project.
+ * @author Nikita
+ */
+public class App 
+{
+    /**
+     * Main satic method of the class.
+     *
+     * @param args - command-line arguments
+     * @throws IOException - input|output exception, failure during reading,
+     *                     writing information
+     */
+    public static void main(String[] args) throws IOException {
+
+//        create view instance
+//        PrimaryView pv = new PrimaryView();
+
+//        create controller instance
+        MajorController mc = new MajorController();
+
+//        run main menu from controller
+//        pv.displayInfo();
+        mc.runMainApplication();
+    }
+}
